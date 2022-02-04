43f405a27607651874ec5785e59bcce9d269b90f
Tomazzo Antonioli <tomazzo10@hotmail.com>
2022-02-04 22:50:15 +0100

    adding tailwind configuration

--- df-client/src/App.js
+++ df-client/src/App.js
@@ -1,5 +1,3 @@
-import "./Styles/App.css";
-
 function App() {
   return (
     <div className="App">
---
+++ df-client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
+  theme: {
+    colors: {
+      greenPrinc: "DFF5DF",
+      greyPrinc: "C4C4C4",
+      companyCol: "F5F5DF",
+      driverCol: "DFEAF5",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
