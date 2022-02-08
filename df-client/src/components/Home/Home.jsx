edee2f528dd8bc7ec82cd8342d6cff053dfb012d
Tomazzo Antonioli <tomazzo10@hotmail.com>
2022-02-08 16:54:00 +0100

    Home: added initial style and organized NavBar

--- df-client/src/components/Home/Home.jsx
+++ df-client/src/components/Home/Home.jsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="flex w-full h-full justify-center items-center bg-greenPrinc">
-      <p>Home</p>
+    <div className="flex flex-col w-full h-full justify-start items-center bg-greenPrinc">
+      <div className="flex h-16 w-full mt-10 justify-center items-center bg-greyPrinc">
+        <h1>DRIVER FINDER</h1>
+      </div>
+      <div className="flex h-16 w-full mt-10 justify-around items-center bg-greyPrinc">
+        <p>Home</p>
+        <p>Login/Register</p>
+      </div>
     </div>
   );
 }
